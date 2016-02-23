655e410fb6bf96a06ffaf8351850049f3e13e16a
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2016-02-24 01:27:48 +0300

    Add laws for type classes

--- Numeric/LALA/VecSpace.hs
+++ Numeric/LALA/VecSpace.hs
@@ -2,10 +2,24 @@
 -- |
 module Numeric.LALA.VecSpace where
 
+-- | Laws for monoid
+--
+--   1. Usual monoid laws
+--
+--   2. If v is also Num
+-- > zeroV = 0
+-- > (.+.) = (+)
 class AdditiveMonoid v where
   zeroV :: v
   (.+.) :: v -> v -> v
 
+-- | Laws for group
+--
+--  1. Usual group laws
+--
+--  2. If v is also Num
+-- > negateV = negate
+-- > (.-.)   = (-)
 class AdditiveMonoid v => AdditiveGroup v where
   negateV :: v -> v
   (.-.)   :: v -> v -> v
@@ -14,5 +28,3 @@ class AdditiveGroup v => VectorSpace v where
   type Scalar v :: *
   (*.) :: Scalar v -> v -> v
   (.*) :: v -> Scalar v -> v
-
-
